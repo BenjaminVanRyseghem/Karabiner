6720ba0d8fc3294093bbdad6457e5bc7da2543b2
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-03-03 21:19:38 +0100

    use caps lock as CTRL_R for nano support

--- exported.sh
+++ exported.sh
@@ -8,11 +8,11 @@ $cli set repeat.initial_wait 0
 /bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
 /bin/echo -n .
+$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
+/bin/echo -n .
 $cli set ben_move_mouse 1
 /bin/echo -n .
 $cli set remap.eject2forwarddelete_retain_fnEject 1
-/bin/echo -n .
-$cli set option.extra_cursor_controlL_jkil_hsemicolon 1
 /bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
 /bin/echo -n .
