0f88f984f2a4df42b4e3378a34bef788b3fffc42
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-03-13 13:06:52 +0100

    Better end and home for WASD

--- exported.sh
+++ exported.sh
@@ -2,18 +2,26 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set repeat.wait 10
+$cli set ben.insert2play 1
 /bin/echo -n .
 $cli set repeat.initial_wait 0
 /bin/echo -n .
-$cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
-/bin/echo -n .
-$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
-/bin/echo -n .
-$cli set ben_move_mouse 1
+$cli set ben.alfred.begin.end 1
 /bin/echo -n .
 $cli set remap.eject2forwarddelete_retain_fnEject 1
 /bin/echo -n .
+$cli set private.swap_KeyCode::PC_APPLICATION_ModifierFlag::FN 1
+/bin/echo -n .
+$cli set ben.alfred.compatibility 1
+/bin/echo -n .
+$cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
+/bin/echo -n .
+$cli set ben_move_mouse 1
+/bin/echo -n .
+$cli set repeat.wait 10
+/bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
+/bin/echo -n .
+$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
 /bin/echo -n .
 /bin/echo
