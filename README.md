8b7bca50659359fe5ade68cb66b6c8b9ada70d0c
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2015-05-13 11:47:34 +0200

    Adds windows tweaks for Maltron

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ This repository holds all my configs and homemade setting for `KeyRemap4MacBook`
 
 The generation of the settings is made with
 
-    /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli export > exported.sh 
+    /Applications/Karabiner.app/Contents/Library/bin/karabiner export > exported.sh  
 
 as explained [here](https://groups.google.com/d/msg/keyremap4macbook/FJ6BO5xzBsE/CYUJQTsKCSQJ)
--- exported.sh
+++ exported.sh
@@ -1,31 +1,45 @@
 #!/bin/sh
 
-cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set ben.insert2play 1
 /bin/echo -n .
-$cli set repeat.initial_wait 0
+$cli set ben.windows.modifier 1
 /bin/echo -n .
-$cli set ben.alfred.begin.end 1
-/bin/echo -n .
-$cli set remap.eject2forwarddelete_retain_fnEject 1
-/bin/echo -n .
-$cli set private.swap_KeyCode::PC_APPLICATION_ModifierFlag::FN 1
-/bin/echo -n .
-$cli set ben.alfred.compatibility 1
+$cli set ben.wasd.alfred.compatibility 1
 /bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
 /bin/echo -n .
-$cli set ben_move_mouse 1
+$cli set ben.maltron 1
 /bin/echo -n .
 $cli set repeat.wait 10
 /bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
 /bin/echo -n .
-$cli set ben.wasd.alfred.compatibility 1
+$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
+/bin/echo -n .
+$cli set private.swap_KeyCode::PC_APPLICATION_ModifierFlag::FN 1
 /bin/echo -n .
 $cli set ben.remap.music 1
 /bin/echo -n .
-$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
+$cli set ben.windows.tabs 1
+/bin/echo -n .
+$cli set ben_move_mouse 1
+/bin/echo -n .
+$cli set ben.alfred.begin.end 1
+/bin/echo -n .
+$cli set ben.maltron.windows.modifier 1
+/bin/echo -n .
+$cli set ben.windows.super 1
+/bin/echo -n .
+$cli set ben.alfred.compatibility 1
+/bin/echo -n .
+$cli set ben.maltron.not.windows.modifier 1
+/bin/echo -n .
+$cli set ben.capslock 1
+/bin/echo -n .
+$cli set ben.maltron.macros 1
+/bin/echo -n .
+$cli set repeat.initial_wait 0
 /bin/echo -n .
 /bin/echo
