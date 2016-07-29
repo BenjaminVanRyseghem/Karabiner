7e396e8d1febb53dbb51f687cb21fc0a43cefe47
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-07-29 10:57:39 +0200

    Update exported.sh

--- exported.sh
+++ exported.sh
@@ -2,50 +2,80 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set ben.insert2play 1
-/bin/echo -n .
-$cli set ben.colemak.liu 1
-/bin/echo -n .
-$cli set ben.windows.modifier 1
-/bin/echo -n .
-$cli set repeat.wait 10
-/bin/echo -n .
-$cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
-/bin/echo -n .
-$cli set ben.wasd.alfred.compatibility 1
-/bin/echo -n .
-$cli set ben.maltron 1
-/bin/echo -n .
-$cli set ben.colemak.tarmak5 1
-/bin/echo -n .
-$cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
-/bin/echo -n .
-$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
-/bin/echo -n .
-$cli set ben.m570.buttons 1
-/bin/echo -n .
-$cli set private.swap_KeyCode::PC_APPLICATION_ModifierFlag::FN 1
-/bin/echo -n .
-$cli set ben.remap.music 1
-/bin/echo -n .
-$cli set ben.windows.tabs 1
-/bin/echo -n .
 $cli set ben_move_mouse 1
 /bin/echo -n .
-$cli set ben.maltron.home.end 1
-/bin/echo -n .
-$cli set ben.maltron.windows.modifier 1
-/bin/echo -n .
-$cli set ben.windows.super 1
+$cli set ben.alfred.begin.end 1
 /bin/echo -n .
 $cli set ben.alfred.compatibility 1
 /bin/echo -n .
-$cli set ben.maltron.not.windows.modifier 1
+$cli set ben.arrows 1
 /bin/echo -n .
 $cli set ben.capslock 1
 /bin/echo -n .
+$cli set ben.colemak.liu 1
+/bin/echo -n .
+$cli set ben.colemak.tarmak5 1
+/bin/echo -n .
+$cli set ben.ergotron.home.end.colemak 1
+/bin/echo -n .
+$cli set ben.ergotron.home.end.qwerty 1
+/bin/echo -n .
+$cli set ben.ergotron.macros.colemak 1
+/bin/echo -n .
+$cli set ben.ergotron.macros.qwerty 1
+/bin/echo -n .
+$cli set ben.insert2play 1
+/bin/echo -n .
+$cli set ben.m570.buttons 1
+/bin/echo -n .
+$cli set ben.m570.setup 1
+/bin/echo -n .
+$cli set ben.maltron 1
+/bin/echo -n .
+$cli set ben.maltron.capslock 1
+/bin/echo -n .
+$cli set ben.maltron.home.end 1
+/bin/echo -n .
+$cli set ben.maltron.home.end.colemak 1
+/bin/echo -n .
 $cli set ben.maltron.macros 1
 /bin/echo -n .
-$cli set repeat.initial_wait 0
+$cli set ben.maltron.macros.colemak 1
+/bin/echo -n .
+$cli set ben.maltron.macros.qwerty 1
+/bin/echo -n .
+$cli set ben.maltron.not.windows.modifier 1
+/bin/echo -n .
+$cli set ben.maltron.windows.modifier 1
+/bin/echo -n .
+$cli set ben.my.capslock 1
+/bin/echo -n .
+$cli set ben.my.hyper 1
+/bin/echo -n .
+$cli set ben.my.music 1
+/bin/echo -n .
+$cli set ben.remap.music 1
+/bin/echo -n .
+$cli set ben.wasd.alfred.compatibility 1
+/bin/echo -n .
+$cli set ben.windows.modifier 1
+/bin/echo -n .
+$cli set ben.windows.super 1
+/bin/echo -n .
+$cli set ben.windows.tabs 1
+/bin/echo -n .
+$cli set option.extra_cursor_controlR_jkil_hsemicolon 1
+/bin/echo -n .
+$cli set private.swap_KeyCode::PC_APPLICATION_ModifierFlag::FN 1
+/bin/echo -n .
+$cli set remap.eject2forwarddelete_retain_fnEject 1
+/bin/echo -n .
+$cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
+/bin/echo -n .
+$cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
+/bin/echo -n .
+$cli set repeat.initial_wait 150
+/bin/echo -n .
+$cli set repeat.wait 10
 /bin/echo -n .
 /bin/echo
