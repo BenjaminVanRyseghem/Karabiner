78c6afb9e167f027735e645b08fb3f4a8214be1e
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-10-10 17:25:26 +0200

    update settings

--- exported.sh
+++ exported.sh
@@ -4,17 +4,11 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set ben_move_mouse 1
 /bin/echo -n .
-$cli set ben.alfred.begin.end 1
-/bin/echo -n .
 $cli set ben.alfred.compatibility 1
-/bin/echo -n .
-$cli set ben.arrows 1
 /bin/echo -n .
 $cli set ben.capslock 1
 /bin/echo -n .
 $cli set ben.colemak.liu 1
-/bin/echo -n .
-$cli set ben.colemak.tarmak5 1
 /bin/echo -n .
 $cli set ben.ergotron.home.end.colemak 1
 /bin/echo -n .
@@ -27,8 +21,6 @@ $cli set ben.ergotron.macros.qwerty 1
 $cli set ben.insert2play 1
 /bin/echo -n .
 $cli set ben.m570.buttons 1
-/bin/echo -n .
-$cli set ben.m570.setup 1
 /bin/echo -n .
 $cli set ben.maltron 1
 /bin/echo -n .
@@ -48,6 +40,8 @@ $cli set ben.maltron.not.windows.modifier 1
 /bin/echo -n .
 $cli set ben.maltron.windows.modifier 1
 /bin/echo -n .
+$cli set ben.mte.buttons 1
+/bin/echo -n .
 $cli set ben.my.capslock 1
 /bin/echo -n .
 $cli set ben.my.hyper 1
@@ -55,8 +49,6 @@ $cli set ben.my.hyper 1
 $cli set ben.my.music 1
 /bin/echo -n .
 $cli set ben.remap.music 1
-/bin/echo -n .
-$cli set ben.wasd.alfred.compatibility 1
 /bin/echo -n .
 $cli set ben.windows.modifier 1
 /bin/echo -n .
@@ -72,9 +64,7 @@ $cli set remap.eject2forwarddelete_retain_fnEject 1
 /bin/echo -n .
 $cli set remap.simultaneouskeypresses_ben_ben_mouse_keys_mode_ds_strict_key_order 1
 /bin/echo -n .
-$cli set remap.simultaneouskeypresses_ben_mouse_keys_mode_ds_strict_key_order 1
-/bin/echo -n .
-$cli set repeat.initial_wait 150
+$cli set repeat.initial_wait 200
 /bin/echo -n .
 $cli set repeat.wait 10
 /bin/echo -n .
